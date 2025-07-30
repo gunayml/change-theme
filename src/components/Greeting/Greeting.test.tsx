a926fea74a3cd42d9c7844b9229667d5198365ff
gunaim <gunay.seyidehmedqizi@gmail.com>
2025-07-30 10:48:35 +0400

    initial commit: React + TypeScript + Vite + Testing setup

---
+++ src/components/Greeting/Greeting.test.tsx
@@ -0,0 +1,7 @@
+import { render, screen } from "@testing-library/react";
+import Greeting from "./Greeting";
+
+test("renders personalized greeting", () => {
+  render(<Greeting name="Gunay" />);
+  expect(screen.getByText("Hello, Gunay!")).toBeInTheDocument();
+});
